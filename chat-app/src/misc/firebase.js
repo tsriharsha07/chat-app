d1c93c72def9de0120c7e9d5a1f73e02a63e9a84
tsriharsha07 <tsriharsha07@gmail.com>
2022-07-04 16:55:05 +0530

    created app

---
+++ chat-app/src/misc/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app'
+
+
+const config={
+    apiKey: "AIzaSyDjlcLhYnF1akBZnMkrJ3z9mb5d1_SI4QI",
+    authDomain: "chat-web-app-cba14.firebaseapp.com",
+    projectId: "chat-web-app-cba14",
+    storageBucket: "chat-web-app-cba14.appspot.com",
+    messagingSenderId: "925637991678",
+    appId: "1:925637991678:web:0ecee56fe5b1144bd259af"
+  };
+
+const app=firebase.initializeApp(config);
